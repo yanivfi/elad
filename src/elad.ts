5ddd20df64effc5ebfc75be478b40fb5c989b873
יניב פישמן <YanivF@eladsystems.co.il>
2018-08-30 11:36:26 +0300

    check precommit

--- src/elad.ts
+++ src/elad.ts
@@ -1,14 +1,22 @@
 export class Elad {
     private _name: string;
     private _version: string;
+    private _age: number;
     constructor() {
         this._name = 'Elad Systems';
         this._version = '0.0.6';
+
     }
     get name() {
         return this._name;
     }
     get version() {
         return this._version;
+    }
+    get age() {
+        return this._age;
+    }
+    set age(value: number) {
+        this._age = value;
     }
 }
